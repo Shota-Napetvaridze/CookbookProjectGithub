c0250430da0ae5baccc1851cb59e77321e4d1f1b
Shota Napetvaridze <sn223ki@student.lnu.se>
2022-05-12 09:59:19 +0200

    New Features :P

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -13,15 +13,21 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         //
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxmlFiles/home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1315.0 , 810.0 ); // TODO: 600 400 (Replace) 1315.0, 810.0
         stage.setTitle("CookBook!");
         stage.setScene(scene);
-        stage.setResizable(false);
+        stage.setResizable(false); // Sets the window size unresizable
         stage.show();
     }
 
 
         public static void main(String[] args) {
+            // Start MAMP server Automatically
+            
+            /*Runtime.getRuntime().exec("open /Applications/MAMP/MAMP.app/");
+            Runtime.getRuntime().exec("/Applications/MAMP/bin/startApache.sh");
+            Runtime.getRuntime().exec("/Applications/MAMP/bin/startMysql.sh");
+            TimeUnit.SECONDS.sleep(5);*/
             DbContext dbContext = new DbContext(Variables.DATABASE_PORT, Variables.DATABASE_USER, Variables.DATABASE_PASS);
             dbContext.createDatabase();
 
