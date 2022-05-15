236c11f6713ff88cd307a27d82be06e44a5d7345
Shota Napetvaridze <sn223ki@student.lnu.se>
2022-05-15 19:04:11 +0200

    working on fxml files

--- app/src/main/java/controllers/UserController.java
+++ app/src/main/java/controllers/UserController.java
@@ -1,4 +1,38 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class UserController {
+
+    @FXML
+    private Button editUser;
+
+    @FXML
+    private Label emailLbl;
+
+    @FXML
+    private Label nicknameLbl;
+
+    @FXML
+    private Label passwordLbl;
+
+    @FXML
+    private Button removeUser;
+
+    @FXML
+    private Label userNameLbl;
+
+    @FXML
+    void edit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void remove(MouseEvent event) {
+
+    }
+
 }
